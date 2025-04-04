d8e1880ac1688667fcd84ed25c75f1035a3de627
LocNguyen512 <tanlocnguyen0512@gmail.com>
2025-04-04 15:50:28 +0700

    First base Frontend

---
+++ src/frontend/my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./component/Header"
+import Footer from "./component/Footer"
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+      
+  )
+}
+export default App
