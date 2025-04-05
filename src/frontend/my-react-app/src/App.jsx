f177b33c986fa22921771c9a646a07ba6cf6e3dc
LocNguyen512 <tanlocnguyen0512@gmail.com>
2025-04-05 20:40:23 +0700

    Update_FE

--- src/frontend/my-react-app/src/App.jsx
+++ src/frontend/my-react-app/src/App.jsx
@@ -1,13 +1,21 @@
-import Header from "./component/Header"
-import Footer from "./component/Footer"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/HomePage/Home';
+import LichThi from './Pages/LichThi/LichThi';
+import BangGia from './Pages/BangGia/BangGia';
+import TraCuuChungChi from './Pages/TraCuuChungChi/TraCuuChungChi';
+
 
 function App() {
   return (
-    <>
-      <Header></Header>
-      <Footer></Footer>
-    </>
-      
-  )
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/BangGia" element={<BangGia />} />
+          <Route path="/LichThi" element={<LichThi />} />
+          <Route path="/ChungChi" element={<TraCuuChungChi />} />
+        </Routes>
+    </Router>
+  );
 }
-export default App
+
+export default App;
