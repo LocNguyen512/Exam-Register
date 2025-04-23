202ac76df7eea43f6a141e169b477190f80d5969
huyen12112k4 <128444657+huyen12112k4@users.noreply.github.com>
2025-04-23 23:37:50 +0700

    Update thư viện

--- src/backend/app.py
+++ src/backend/app.py
@@ -11,7 +11,6 @@ app.config.from_object(Config)
 db.init_app(app)
 
 app.register_blueprint(auth_bp, url_prefix="/auth")
-app.register_blueprint(nvtn_bp, url_prefix="/nhanvientn")
 @app.route('/')
 def index():
     return "Hello, World!"
